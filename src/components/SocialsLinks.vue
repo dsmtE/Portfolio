87f1054da9349cbfbd3aba3e9095e22621d86556
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2020-10-14 21:24:06 +0200

    [Component] Socials Links

---
+++ src/components/SocialsLinks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="column">
+      <div v-for="(item, id) in items" :key="id" class="col q-mb-sm">
+        <q-btn flat :icon="item.icon" type="a" target="_blank" :href="item.url" />
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SocialsLinks',
+  components: {
+  },
+  props: {
+    items: {
+      type: Array,
+      require: true,
+      validator: array => array.every(item => item.icon && item.url),
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
